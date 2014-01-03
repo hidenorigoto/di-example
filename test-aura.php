097bcb5e6dba314520f0ce4624e9d9f0ed5a556c
hidenorigoto <hidenorigoto@gmail.com>
2014-01-03 09:56:09 +0900

    added test scripts for aura-di and zend-di

---
+++ test-aura.php
@@ -0,0 +1,29 @@
+<?php
+require_once "vendor/autoload.php";
+
+class Single {
+
+}
+
+class Dependent {
+    /**
+     * @var Single
+     */
+    private $s;
+    public function __construct(Single $s)
+    {
+        $this->s = $s;
+    }
+}
+
+use Aura\Di\Container;
+use Aura\Di\Forge;
+use Aura\Di\Config;
+
+$di = new Container(new Forge(new Config));
+
+$single = $di->newInstance('Single');
+var_dump($single);
+
+$dependent = $di->newInstance('Dependent'); // causes fatal error
+var_dump($dependent);
---
+++ test-zend.php
@@ -0,0 +1,25 @@
+<?php
+require_once "vendor/autoload.php";
+
+class Single {
+
+}
+
+class Dependent {
+    /**
+     * @var Single
+     */
+    private $s;
+    public function __construct(Single $s)
+    {
+        $this->s = $s;
+    }
+}
+
+$di = new Zend\Di\Di();
+
+$single = $di->newInstance('Single');
+var_dump($single);
+
+$dependent = $di->newInstance('Dependent');
+var_dump($dependent);
